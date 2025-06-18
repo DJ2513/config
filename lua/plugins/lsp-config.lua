db0f0ec4aa23d423bc0ee9e062811943066aef21
Diego <djontiveros2728@gmail.com>
2025-06-18 12:13:04 -0600

    My nvim configurations

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "ast_grep", "jdtls", "lua_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.ast_grep.setup({})
+            lspconfig.jdtls.setup({})
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+}
