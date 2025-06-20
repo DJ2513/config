c3a735115b9320acf89326461b315ae4c48baa8d
Diego <djontiveros2728@gmail.com>
2025-06-20 13:50:51 -0600

    Golang formatters and diagnotics settings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "ast_grep", "jdtls", "lua_ls" },
+                ensure_installed = { "ast_grep", "jdtls", "lua_ls", "gopls" },
             })
         end,
     },
@@ -20,6 +20,17 @@ return {
             lspconfig.ast_grep.setup({})
             lspconfig.jdtls.setup({})
             lspconfig.lua_ls.setup({})
+            lspconfig.gopls.setup({
+                settings ={
+                    gopls={
+                        analyze = {
+                            unusedparams = true,
+                            shadow = true,
+                        },
+                        staticcheck = true,
+                    },
+                },
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,11 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.google_java_format,
+                null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.diagnostics.eslint,
+                null_ls.builtins.diagnostics.golangci_lint,
                 null_ls.builtins.diagnostics.checkstyle.with({
                     extra_args = { "-c", "google_checks.xml" },
                 }),
