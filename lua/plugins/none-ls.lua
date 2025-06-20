6dd7c62d2a1787368c7ac363173a1043b9f4f28b
Diego Jimenez <djontiveros2728@gmail.com>
2025-06-20 14:10:00 -0600

    Added missing formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,7 @@ return {
                 null_ls.builtins.formatting.gofmt,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.goimports,
-                null_ls.builtins.diagnostics.eslint,
+                null_ls.builtins.diagnostics.eslint_d,
                 null_ls.builtins.diagnostics.golangci_lint,
                 null_ls.builtins.diagnostics.checkstyle.with({
                     extra_args = { "-c", "google_checks.xml" },
