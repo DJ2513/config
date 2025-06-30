3f4749c5f1838a7de46e9ce718f4783879f39d53
Diego Jimenez <djontiveros2728@gmail.com>
2025-06-30 16:35:16 -0600

    Errors and warnings showing on hover

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,39 +1,62 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "ast_grep", "jdtls", "lua_ls", "gopls" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.ast_grep.setup({})
-            lspconfig.jdtls.setup({})
-            lspconfig.lua_ls.setup({})
-            lspconfig.gopls.setup({
-                settings ={
-                    gopls={
-                        analyze = {
-                            unusedparams = true,
-                            shadow = true,
-                        },
-                        staticcheck = true,
-                    },
-                },
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "ast_grep", "jdtls", "lua_ls", "gopls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local on_attach = function(_, bufnr)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { buffer = bufnr })
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+				vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist, {})
+			end
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = function()
+					vim.diagnostic.open_float(nil, {
+						focusable = false,
+						close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+						border = "rounded",
+						source = "always",
+						prefix = " ",
+						scope = "cursor",
+					})
+				end,
+			})
+			lspconfig.ast_grep.setup({})
+			lspconfig.jdtls.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+				settings = {
+					gopls = {
+						analyses = {
+							unusedparams = true,
+							shadow = true,
+						},
+						staticcheck = true,
+						completeUnimported = true,
+						usePlaceholders = true,
+					},
+				},
+			})
+			lspconfig.pyright.setup({})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,21 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.gofmt,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.goimports,
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.golangci_lint,
-                null_ls.builtins.diagnostics.checkstyle.with({
-                    extra_args = { "-c", "google_checks.xml" },
-                }),
-            },
-        })
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.diagnostics.ruff,
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.diagnostics.checkstyle.with({
+					extra_args = { "-c", "google_checks.xml" },
+				}),
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
