6b8fbaf861b4321733ad88a315cc15837532a9cd
Diego Jimenez <djontiveros2728@gmail.com>
2025-07-01 18:14:46 -0600

    corrected some time issues

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set encoding=utf-8")
 vim.cmd("set rnu")
-vim.cmd("set updatetime=300") --300ms
+vim.cmd("set updatetime=500") --500ms
 vim.g.mapleader = " "
 
 
